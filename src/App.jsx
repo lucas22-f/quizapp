6866bc280b0a74efd1580cfaf6e14d60c61b6237
lucas <lucas.200061@gmail.com>
2024-02-01 11:29:15 -0300

    path

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import Title from './components/Title/Title'
 import GameBanner from './components/GameBanner/GameBanner'
-import Ruleta from './components/Ruleta/Ruleta'
+
 
 
 function App() {
